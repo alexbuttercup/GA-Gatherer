7449a9b950558d399c2aa00874653f0b479c0497
alexbuttercup <onerusin@gmail.com>
2019-10-22 14:43:11 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,20 @@
-  (function () {
+//input filter for domains
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+//initializing tablesorter
+$(function() {
+  $(".bigtable").tablesorter();
+});
+
+//material design button
+(function () {
     "use strict";
 
     var button = document.querySelector('.material-bloom'),
